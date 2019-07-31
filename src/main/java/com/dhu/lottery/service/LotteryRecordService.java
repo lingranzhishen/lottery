6aeaa7e527ce733f079caf805c7369d1880f62ed
lingranzhishen <h523588>
2019-08-01 00:28:46 +0800

    120监控

--- src/main/java/com/dhu/lottery/service/LotteryRecordService.java
+++ src/main/java/com/dhu/lottery/service/LotteryRecordService.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,123 +25,171 @@ import com.dhu.lottery.rule.ILotteryRule;
 
 @Service
 public class LotteryRecordService {
-	@Autowired
-	LotteryRecordDao lotteryRecordDao;
-	@Autowired
-	RestTemplate restTemplate;
-	@Autowired
-	HttpUtil httpUtil;
+    @Autowired
+    LotteryRecordDao lotteryRecordDao;
+    @Autowired
+    RestTemplate restTemplate;
+    @Autowired
+    HttpUtil httpUtil;
 
-	public String getLotteryMiss() {
-		List<LotteryRecord> records = lotteryRecordDao.getTodayLotteryRecord();
-		List<LotteryRule> rules = lotteryRecordDao.getAllRule();
-		List<ILotteryRule> ruleList = new ArrayList<>();
-		if (rules != null) {
-			for (LotteryRule lr : rules) {
-				ILotteryRule ruleBean = (ILotteryRule) SpringContextUtil.getBean(lr.getRuleCode());
-				if (ruleBean != null) {
-					ruleBean.setLotteryRule(lr);
-					ruleList.add(ruleBean);
-				}
-			}
-		}
-		StringBuilder result = new StringBuilder();
-		for (ILotteryRule ilr : ruleList) {
-			if (ilr.isMatch(records)) {
-				result.append(ilr.getRuleResult()).append(";");
-			}
-		}
-		if (result.length() > 0) {
-			return result.toString();
-		}
-		return StringUtil.EMPTY;
-	}
+    public String getLotteryMiss() {
+        List<LotteryRecord> records = lotteryRecordDao.getTodayLotteryRecord();
+        List<LotteryRule> rules = lotteryRecordDao.getAllRule();
+        List<ILotteryRule> ruleList = new ArrayList<>();
+        if (rules != null) {
+            for (LotteryRule lr : rules) {
+                ILotteryRule ruleBean = (ILotteryRule) SpringContextUtil.getBean(lr.getRuleCode());
+                if (ruleBean != null) {
+                    ruleBean.setLotteryRule(lr);
+                    ruleList.add(ruleBean);
+                }
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (ILotteryRule ilr : ruleList) {
+            if (ilr.isMatch(records)) {
+                result.append(ilr.getRuleResult()).append(";");
+            }
+        }
+        if (result.length() > 0) {
+            return result.toString();
+        }
+        return StringUtil.EMPTY;
+    }
 
-	public List<LotteryRecord> getTodayLotteryRecord() {
-		return lotteryRecordDao.getTodayLotteryRecord();
-	}
+    public List<LotteryRecord> getTodayLotteryRecord() {
+        return lotteryRecordDao.getTodayLotteryRecord();
+    }
 
-	@Deprecated
-	public String insertLotteryRecord() {
-		try {
+    @Deprecated
+    public String insertLotteryRecord() {
+        try {
 
-			String result = httpUtil
-					.doGet("http://caipiao.163.com/award/getAwardNumberInfo.html?gameEn=ssc&periodNum=10");
-			JSONObject jo = JSONObject.parseObject(result);
-			JSONArray awards = jo.getJSONArray("awardNumberInfoList");
-			String lotteryNo = StringUtil.EMPTY;
-			for (int i = 1; i < awards.size(); i++) {
-				JSONObject award = awards.getJSONObject(i);
-				LotteryRecord lotteryRecord = new LotteryRecord();
-				String lastestPhase = award.getString("period");
-				if (lotteryRecordDao.exists(lastestPhase) < 1) {
-					String lastestNumber = award.getString("winningNumber").replaceAll(" ", StringUtil.EMPTY);
-					lotteryRecord.setCreateTime(new Date());
-					lotteryRecord.setLotteryNo(lastestPhase);
-					lotteryRecord.setSequenceOfToday(Integer.parseInt(lastestPhase.substring(6)));
-					lotteryRecord.setNumber(lastestNumber);
-					lotteryRecord.setFirstDigit(lastestNumber.charAt(0) - '0');
-					lotteryRecord.setSecondDigit(lastestNumber.charAt(1) - '0');
-					lotteryRecord.setThirdDigit(lastestNumber.charAt(2) - '0');
-					lotteryRecord.setFourthDigit(lastestNumber.charAt(3) - '0');
-					lotteryRecord.setFifthDigit(lastestNumber.charAt(4) - '0');
-					lotteryRecordDao.insertLotteryRecord(lotteryRecord);
-					lotteryNo = lotteryRecord.getLotteryNo();
-				}
-			}
-			return lotteryNo;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return StringUtil.EMPTY;
-	}
-
-
-	public String insertLotteryRecordV2() {
-		try {
-
-			String result = httpUtil
-					.doGet("https://www.k8kjw9.com/api/recent?code=cq_ssc");
-			JSONObject jo = JSONObject.parseObject(result);
-			JSONArray awards = jo.getJSONArray("data");
-			String lotteryNo = StringUtil.EMPTY;
-			for (int i = 0; i < awards.size(); i++) {
-				JSONObject award = awards.getJSONObject(i);
-				LotteryRecord lotteryRecord = new LotteryRecord();
-				String lastestPhase = award.getString("issue").substring(2);
-				if (lotteryRecordDao.exists(lastestPhase) < 1) {
-					String lastestNumber = award.getString("code").replaceAll(",", StringUtil.EMPTY);
-					lotteryRecord.setCreateTime(new Date());
-					lotteryRecord.setLotteryNo(lastestPhase);
-					lotteryRecord.setSequenceOfToday(Integer.parseInt(lastestPhase.substring(6)));
-					lotteryRecord.setNumber(lastestNumber);
-					lotteryRecord.setFirstDigit(lastestNumber.charAt(0) - '0');
-					lotteryRecord.setSecondDigit(lastestNumber.charAt(1) - '0');
-					lotteryRecord.setThirdDigit(lastestNumber.charAt(2) - '0');
-					lotteryRecord.setFourthDigit(lastestNumber.charAt(3) - '0');
-					lotteryRecord.setFifthDigit(lastestNumber.charAt(4) - '0');
-					lotteryRecordDao.insertLotteryRecord(lotteryRecord);
-					lotteryNo = lotteryRecord.getLotteryNo();
-				}
-			}
-			return lotteryNo;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return StringUtil.EMPTY;
-	}
+            String result = httpUtil
+                    .doGet("http://caipiao.163.com/award/getAwardNumberInfo.html?gameEn=ssc&periodNum=10");
+            JSONObject jo = JSONObject.parseObject(result);
+            JSONArray awards = jo.getJSONArray("awardNumberInfoList");
+            String lotteryNo = StringUtil.EMPTY;
+            for (int i = 1; i < awards.size(); i++) {
+                JSONObject award = awards.getJSONObject(i);
+                LotteryRecord lotteryRecord = new LotteryRecord();
+                String lastestPhase = award.getString("period");
+                if (lotteryRecordDao.exists(lastestPhase) < 1) {
+                    String lastestNumber = award.getString("winningNumber").replaceAll(" ", StringUtil.EMPTY);
+                    lotteryRecord.setCreateTime(new Date());
+                    lotteryRecord.setLotteryNo(lastestPhase);
+                    lotteryRecord.setSequenceOfToday(Integer.parseInt(lastestPhase.substring(6)));
+                    lotteryRecord.setNumber(lastestNumber);
+                    lotteryRecord.setFirstDigit(lastestNumber.charAt(0) - '0');
+                    lotteryRecord.setSecondDigit(lastestNumber.charAt(1) - '0');
+                    lotteryRecord.setThirdDigit(lastestNumber.charAt(2) - '0');
+                    lotteryRecord.setFourthDigit(lastestNumber.charAt(3) - '0');
+                    lotteryRecord.setFifthDigit(lastestNumber.charAt(4) - '0');
+                    lotteryRecordDao.insertLotteryRecord(lotteryRecord);
+                    lotteryNo = lotteryRecord.getLotteryNo();
+                }
+            }
+            return lotteryNo;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return StringUtil.EMPTY;
+    }
 
 
+    public String insertLotteryRecordV3() {
+        try {
+            String result = httpUtil
+                    .doGet("https://shishicai.cjcp.com.cn/chongqing/kaijiang");
+            Document doc = Jsoup.parse(result);
+            String lotteryNo = StringUtil.EMPTY;
+            Elements kjjg_table = doc.getElementsByClass("kjjg_table");
+            Element table = kjjg_table.first();
+            Elements trs = table.select("tr");
 
-	public List<LotteryMiss> getAllLotteryMiss() {
-		return lotteryRecordDao.getAllLotteryMiss();
-	}
+            for (int i = 0; i < trs.size(); i++) {
+                String lastestPhase = "";
+                String lastestNumber = "";
+                Element tr = trs.get(i);
+                Elements tds = tr.select("td");
+                if (tds.get(0).hasClass("hui")) {
+                    break;
+                }
+                lastestPhase = tds.get(0).text().substring(2, 9);
+                Elements numbers = tds.get(3).select("hm_bg");
+                for (int j = 0; j < numbers.size(); ++j) {
+                    Element td = numbers.get(j);
+                    lastestNumber += td.text();
+                }
 
-	public void insertLotteryMiss(LotteryMiss lm) {
-		lotteryRecordDao.insertLotteryMiss(lm);
-	}
+                LotteryRecord lotteryRecord = new LotteryRecord();
+                if (lotteryRecordDao.exists(lastestPhase) < 1) {
+                    lotteryRecord.setCreateTime(new Date());
+                    lotteryRecord.setLotteryNo(lastestPhase);
+                    lotteryRecord.setSequenceOfToday(Integer.parseInt(lastestPhase.substring(6)));
+                    lotteryRecord.setNumber(lastestNumber);
+                    lotteryRecord.setFirstDigit(lastestNumber.charAt(0) - '0');
+                    lotteryRecord.setSecondDigit(lastestNumber.charAt(1) - '0');
+                    lotteryRecord.setThirdDigit(lastestNumber.charAt(2) - '0');
+                    lotteryRecord.setFourthDigit(lastestNumber.charAt(3) - '0');
+                    lotteryRecord.setFifthDigit(lastestNumber.charAt(4) - '0');
+                    lotteryRecordDao.insertLotteryRecord(lotteryRecord);
+                    lotteryNo = lotteryRecord.getLotteryNo();
+                }
 
-	public int updateLotteryMiss() {
-		return lotteryRecordDao.updateLotteryMiss();
-	}
+            }
+            return lotteryNo;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return StringUtil.EMPTY;
+    }
+
+
+    public String insertLotteryRecordV2() {
+        try {
+
+            String result = httpUtil
+                    .doGet("https://www.k8kjw9.com/api/recent?code=cq_ssc");
+            JSONObject jo = JSONObject.parseObject(result);
+            JSONArray awards = jo.getJSONArray("data");
+            String lotteryNo = StringUtil.EMPTY;
+            for (int i = 0; i < awards.size(); i++) {
+                JSONObject award = awards.getJSONObject(i);
+                LotteryRecord lotteryRecord = new LotteryRecord();
+                String lastestPhase = award.getString("issue").substring(2);
+                if (lotteryRecordDao.exists(lastestPhase) < 1) {
+                    String lastestNumber = award.getString("code").replaceAll(",", StringUtil.EMPTY);
+                    lotteryRecord.setCreateTime(new Date());
+                    lotteryRecord.setLotteryNo(lastestPhase);
+                    lotteryRecord.setSequenceOfToday(Integer.parseInt(lastestPhase.substring(6)));
+                    lotteryRecord.setNumber(lastestNumber);
+                    lotteryRecord.setFirstDigit(lastestNumber.charAt(0) - '0');
+                    lotteryRecord.setSecondDigit(lastestNumber.charAt(1) - '0');
+                    lotteryRecord.setThirdDigit(lastestNumber.charAt(2) - '0');
+                    lotteryRecord.setFourthDigit(lastestNumber.charAt(3) - '0');
+                    lotteryRecord.setFifthDigit(lastestNumber.charAt(4) - '0');
+                    lotteryRecordDao.insertLotteryRecord(lotteryRecord);
+                    lotteryNo = lotteryRecord.getLotteryNo();
+                }
+            }
+            return lotteryNo;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return StringUtil.EMPTY;
+    }
+
+
+    public List<LotteryMiss> getAllLotteryMiss() {
+        return lotteryRecordDao.getAllLotteryMiss();
+    }
+
+    public void insertLotteryMiss(LotteryMiss lm) {
+        lotteryRecordDao.insertLotteryMiss(lm);
+    }
+
+    public int updateLotteryMiss() {
+        return lotteryRecordDao.updateLotteryMiss();
+    }
 }
